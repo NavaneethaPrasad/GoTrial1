14e46e9d2a8e16439c0208a58b3e3914e44b6d44
NavaneethaPrasad <navaneethaprasad25092002@gmail.com>
2025-10-13 20:35:15 +0530

    add test program

---
+++ squirrel_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPhi(t *testing.T) {
+	c := counts{n00: 1, n01: 1, n10: 1, n11: 1}
+	if phi(c) != 0.0 {
+		t.Errorf("Expected 0.0 but got %f\n", phi(c))
+	}
+	c = counts{n00: 0, n01: 5, n10: 5, n11: 0}
+	if phi(c) != -1.0 {
+		t.Errorf("Expected -1.0 but got %f\n", phi(c))
+	}
+	c = counts{n00: 5, n01: 0, n10: 0, n11: 5}
+	if phi(c) != 1.0 {
+		t.Errorf("Expected 1.0 but got %f\n", phi(c))
+	}
+}
